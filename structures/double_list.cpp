d38847e1c8c1a3ffb4bcd762c3bb4f8007fcd74f
m <milanadya4kova@yandex.ru>
2026-01-10 23:09:35 +0700

    Update double_list.cpp

--- structures/double_list.cpp
+++ structures/double_list.cpp
@@ -16,6 +16,50 @@ void Add(Node*& head, int value) {
     head = newNode;
 }
 
+void deleteBeg(Node*& head) {
+    if (head == nullptr) {
+        return;
+    }
+
+    Node* deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+}
+
+void AddEnd(Node*& head, int value) {
+    Node* newNode = new Node;
+    newNode->data = value;
+    newNode->next = nullptr;
+    newNode->prev = nullptr;
+
+    if (head == nullptr) {
+        head = newNode;
+        return;
+    }
+
+    Node* current = head;
+    while (current->next != nullptr) {
+        current = current->next;
+    }
+
+    current->next = newNode;
+    newNode->prev = current;
+}
+
+void DeleteEnd(Node*& head) {
+    if (head == nullptr) {
+        return;
+    }
+
+    Node* current = head;
+    while (current->next != nullptr) {
+        current = current->next;
+    }
+    
+    current->prev->next = nullptr;  
+    delete current;
+}
+
 void Print(Node*& head) {
     Node* printNode = head;
     while (printNode != nullptr) {
@@ -30,5 +74,13 @@ int main()
     Node* Flist = nullptr; 
     Add(Flist, 4);
     Add(Flist, 5);
+    Print(Flist);
+    AddEnd(Flist, 9);
+    Print(Flist);
+
+    deleteBeg(Flist);
+    Print(Flist);
+
+    DeleteEnd(Flist);
     Print(Flist);
 }
